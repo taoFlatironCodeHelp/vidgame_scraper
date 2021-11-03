eebfbdaa7483eec2a9c67a971670ea244e4260d6
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-11-02 21:46:44 -0500

    Made a loop

--- lib/vidgame_scraper/cli.rb
+++ lib/vidgame_scraper/cli.rb
@@ -3,7 +3,7 @@ class VidgameScraper::CLI
     def start   #instance method
         puts "\n         Welcome to my".colorize(:white) +  " CLI!".colorize(:blue)
         puts "What category do you want deals on today?".colorize(:white)
-        puts " \n [1] Video Games,".colorize(:white) + " [2] Tickets,".colorize(:white) + " [3] Furniture".colorize(:white) + " or" + " Exit".colorize(:red)
+        puts " \n[1] Video Games,".colorize(:white) + " [2] Tickets,".colorize(:white) + " [3] Furniture".colorize(:white) + " or" + " Exit".colorize(:red)
         puts "\nType either" + " '1',".colorize(:white) + "'2',".colorize(:white) + "'3'".colorize(:white) + " or" +  " 'Exit'.".colorize(:red)
 
         input = gets.strip.downcase
@@ -15,24 +15,55 @@ class VidgameScraper::CLI
             categoires = VidgameScraper::Scraper.scrap_catagories(url)
             puts "Link: #{categoires[19].name}"
             puts "URL: #{categoires[19].url}"
+            sub_out
+            
+
     
 
         when "2"
             puts "In" + " Tickets".colorize(:green)
+            url = "https://craigslist.org/"
+            categoires = VidgameScraper::Scraper.scrap_catagories(url)
+            puts "Link: #{categoires[15].name}"
+            puts "URL: #{categoires[15].url}"
+            sub_out
+
 
 
         when "3"
             puts "In" + " Furniture".colorize(:green)
-
+            sub_out
+            
 
         when "exit"
             puts "Goodbye!".colorize(:red)
         else
             puts "Sorry invalid answer".colorize(:red)
+            sub_out
+            
             #they input something incorrect
         end
+    
+    end
+end
 
+
+    def sub_out 
+        puts "Would like to see the list again?".colorize(:white) + " (" + "'y'".colorize(:green) + "/" + "'n'".colorize(:red) + ")" 
+        answers = gets.strip.downcase
+        case answers
+        when "y"
+            start
+        when "n"
+            exit_program
+        else
+            puts "Invalid input. Please try again".colorize(:red)
+            sub_out
+        end
     end
 
 
+
+def exit_program
+    puts "Goodbye!".colorize(:red)
 end
